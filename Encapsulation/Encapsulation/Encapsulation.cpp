2c10d37868f664d74bf1927a7e79924f993bf744
EliaMaillard-Salin <147042560+EliaMaillard-Salin@users.noreply.github.com>
2024-10-09 12:35:53 +0200

    [ADD] SDL

--- Encapsulation/Encapsulation/Encapsulation.cpp
+++ Encapsulation/Encapsulation/Encapsulation.cpp
@@ -4,25 +4,51 @@
 #include <iostream>
 #include <vector>
 #include "test.h"
-
+#include "Integer.h"
 #include "Entities.h"
 #include "World.h"
 
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
 int main()
 {
-	Vector2 vect = Vector2();
-	Vector2 add = Vector2();
 
-	Vector2 result = vect + add;
-	result *= 2;
-	result += vect;
+	// Pointers to our window and surface
+	SDL_Surface* winSurface = NULL;
+	SDL_Window* window = NULL;
+
+	// Initialize SDL. SDL_Init will return -1 if it fails.
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		std::cout << "Error initializing SDL: " << SDL_GetError() << std::endl;
+		system("pause");
+		// End the program
+		return 1;
+	}
+
+	//int a = 0; 
+	//Integer b = Integer(3);
+	//Integer testa = Integer(5);
+	//Integer testc = Integer(10);
+	//std::cout << b + 3 << std::endl;
+	//std::cout << testa + testc << std::endl;
+	//std::cout << testa * 2 << std::endl;
+	//std::cout << testc / testa << std::endl;
+	//std::cout << testc % b << std::endl;
+
+	//Vector2 vect = Vector2();
+	//Vector2 add = Vector2();
+
+	//Vector2 result = vect + add;
+	//result *= 2;
+	//result += vect;
 
 
-	World world = World();
-	world.Init();
-	while (world.HasEnded() == false)
-		world.Step();
-	std::cout << "Simulation Finished !";
+	//World world = World();
+	//world.Init();
+	//while (world.HasEnded() == false)
+	//	world.Step();
+	//std::cout << "Simulation Finished !";
 }
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
